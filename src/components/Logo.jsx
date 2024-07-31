2539696d05140a09a05ebd0c3c48d458aad4a227
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-07-31 23:57:49 +0600

    logo alt update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import image from '../../public/logo.png'
 export default function Logo() {
   return (
     <div className='flex items-center font-bold text-2xl '>
-    <img src={image} className='aspect-square w-10' alt="logo_image" />
+    <img src={image} className='aspect-square w-10' alt="mobile_logo_image" />
     <span className='text-[#ff00d3]'>Mobi</span>
     <span>Shop</span>
     </div>
