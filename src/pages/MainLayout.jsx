782b482fe990b0a909647fcaed8fa226d5ed956f
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-07-31 22:58:59 +0600

    navbar added to main layout

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 
 export default function MainLayout() {
   return (
-    <>Home</>
+    <main>
+      <Navbar />
+      <Outlet />
+      
+    </main>
   )
 }
