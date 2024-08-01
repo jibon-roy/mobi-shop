327ffdcef82955ea1981d2794bb811662419bae6
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-01 23:35:24 +0600

    landing card update

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -6,7 +6,7 @@ export default function LandingCard({mobile}) {
             <img src={mobile?.image} className="w-[80%] transform transition duration-300 group-hover:scale-x-[-1]" alt="Mobile" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
+          <h2 className="card-title">{mobile.name}</h2>
           <p>Click the button to watch on Jetflix app.</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Watch</button>
