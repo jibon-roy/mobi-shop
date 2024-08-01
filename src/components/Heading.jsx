8452b823354c7c84b9271e310ff0c9d9455e519d
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-01 13:34:31 +0600

    heading added

---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+export default function Heading({heading, children}) {
+  return (
+    <div>
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">{heading}</h1>
+        <p className="py-6">
+          {children}
+        </p>
+      </div>
+    </div>
+  );
+}
