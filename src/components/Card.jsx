81a72a85b9b080d1bfa57879ef60b8c8783678e8
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-01 15:10:11 +0600

    card updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 export default function Card({ mobile }) {
-  return (
-
+  
+    const description = mobile?.description
+    const words = description.slice(0,50)
+    return (
       <div className="card mx-auto h-full hover:scale-95 transition-all bg-base-100 max-w-72 shadow-xl">
         <figure>
           <img src={mobile?.image} className="w-1/2" alt="Shoes" />
@@ -8,7 +10,7 @@ export default function Card({ mobile }) {
         <div className="card-body">
           <h2 className="card-title">{mobile.name}</h2>
           <div className="badge badge-outline">{mobile.category}</div>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>{words}...</p>
           <div className="card-actions justify-start">
             <button className="btn btn-secondary">Details</button>
           </div>
