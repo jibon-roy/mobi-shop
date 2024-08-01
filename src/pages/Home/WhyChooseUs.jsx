d2ca0a975647d34137d08a9df802aec355c4469f
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-02 01:13:16 +0600

    why choose us created

--- src/pages/Home/WhyChooseUs.jsx
+++ src/pages/Home/WhyChooseUs.jsx
@@ -1,13 +1,38 @@
-
+import Heading from "../../components/Heading";
+import imagebg from "../../assets/choose.jpg";
 
 export default function WhyChooseUs() {
   return (
     <div>
-        Why Choose Us?
-Wide Selection: We offer the latest mobile phones from top brands at competitive prices.
-Secure Shopping: Enjoy a safe and convenient shopping experience with multiple payment options.
-Fast Delivery: Get your new phone quickly with our efficient delivery service.
-Customer Support: Our dedicated team is here to assist you with any questions or concerns.
+      <div className="container py-20 pb-28">
+        <div className="flex justify-center mb-16">
+          <Heading heading={"Why Choose Us?"}>Make your good decition.</Heading>
+        </div>
+        <div className="flex justify-evenly flex-col lg:flex-row">
+          <img
+            src={imagebg}
+            className="max-w-md h-80 my-auto object-cover rounded-lg "
+          />
+          <div className="max-w-lg my-auto flex-1 text-xl">
+            <p className="mb-5">
+            <strong>Wide Selection:</strong> We offer the latest mobile phones from top brands at
+            competitive prices.
+            </p>
+           <p className="mb-5">
+           <strong>Secure Shopping:</strong> Enjoy a safe and convenient
+           shopping experience with multiple payment options.
+           </p>
+           <p className="mb-5">
+           <strong>Fast Delivery:</strong>
+           Get your new phone quickly with our efficient delivery service.
+           </p>
+          <p className="mb-5">
+          <strong>Customer Support:</strong> Our dedicated team is here to assist you with any
+          questions or concerns.
+          </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
