4a295ce77bb302504935a94c0f7a2895a72c7c4a
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-01 13:32:24 +0600

    footer update

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-
 export default function MainLayout() {
   return (
-    <main>
-      <Navbar />
-      <Outlet />
+    <main className="flex min-h-screen flex-col justify-between">
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+      <div>
       <Footer />
+      </div>
     </main>
-  )
+  );
 }
