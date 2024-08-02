75ddcdc49a167f2d6e0b357f2c0bc7fc87939ce6
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-02 22:16:05 +0600

    router updated

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -5,6 +5,7 @@ import Home from "../pages/Home/Home";
 import Mobiles from "../pages/Mobiles/Mobiles";
 import AboutUs from "../pages/AboutUs/AboutUs";
 import ContactUs from "../pages/Contact/Contact";
+import MobileDetails from "../pages/MobileDetails/MobileDetails";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,15 @@ const router = createBrowserRouter([
         path: "/mobiles",
         element: <Mobiles />,
         loader: async () => await fetch("./phones.json"),
+      },
+      {
+        path: "/mobiles/:id",
+        element: <MobileDetails />,
+        loader: async ({ params }) => {
+          const data = await fetch("./phones.json");
+          const mobiles = data.json();
+          return mobiles.find((a) => a.id == params.id);
+        },
       },
       {
         path: "/about-us",
