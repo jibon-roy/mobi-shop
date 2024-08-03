c7295a88524e1b820e11c0d6057eb192d96c82bd
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-03 20:15:06 +0600

    main layout updating #2

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useState } from "react";
-import { useEffect } from "react";
 
 export default function MainLayout() {
   return (
