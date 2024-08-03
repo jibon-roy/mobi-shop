fa75d8165ac12ff53f11f1373d2e6bb0f451c5a6
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-03 11:21:38 +0600

    responsive issue fixing

--- src/pages/Home/WhyChooseUs.jsx
+++ src/pages/Home/WhyChooseUs.jsx
@@ -11,25 +11,25 @@ export default function WhyChooseUs() {
         <div className="flex justify-evenly items-center gap-10 flex-col lg:flex-row">
           <img
             src={imagebg}
-            className="max-w-md h-80 my-auto object-cover rounded-lg "
+            className="max-w-md w-[80%] sm:w-full max-h-80 my-auto object-cover rounded-lg "
           />
           <div className="max-w-lg my-auto flex-1 text-xl">
             <p className="mb-5">
-            <strong>Wide Selection:</strong> We offer the latest mobile phones from top brands at
-            competitive prices.
+              <strong>Wide Selection:</strong> We offer the latest mobile phones
+              from top brands at competitive prices.
             </p>
-           <p className="mb-5">
-           <strong>Secure Shopping:</strong> Enjoy a safe and convenient
-           shopping experience with multiple payment options.
-           </p>
-           <p className="mb-5">
-           <strong>Fast Delivery:</strong>
-           Get your new phone quickly with our efficient delivery service.
-           </p>
-          <p className="mb-5">
-          <strong>Customer Support:</strong> Our dedicated team is here to assist you with any
-          questions or concerns.
-          </p>
+            <p className="mb-5">
+              <strong>Secure Shopping:</strong> Enjoy a safe and convenient
+              shopping experience with multiple payment options.
+            </p>
+            <p className="mb-5">
+              <strong>Fast Delivery:</strong>
+              Get your new phone quickly with our efficient delivery service.
+            </p>
+            <p className="mb-5">
+              <strong>Customer Support:</strong> Our dedicated team is here to
+              assist you with any questions or concerns.
+            </p>
           </div>
         </div>
       </div>
