5a74a3a4bc5e6d9ca34a9f6795c4b31c07f32089
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-03 20:36:38 +0600

    mainlayout updating

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Loading from "../components/Loading";
 
 export default function MainLayout() {
+  const phoneData = async () => await fetch("./phones.json");
+  // const data = phoneData.json();
+  // console.log(data);
+
+  if (!phoneData) return <Loading />;
   return (
     <main className="flex min-h-screen flex-col justify-between">
       <div>
