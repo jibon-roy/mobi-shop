dd30f7547221916f7e0b4da7be7cbd34cd705246
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-03 20:21:39 +0600

    loading updated #5

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,7 @@
 const Loading = () => {
   return (
-    <div className="flex items-center z-50 justify-center h-screen bg-gray-100">
-      <div className="w-64 bg-gray-300 rounded-full h-6">
-        <span className="loading loading-ball loading-lg"></span>
-      </div>
+    <div className="flex items-center w-full z-50 justify-center h-screen bg-gray-100">
+      <span className="loading loading-ball loading-lg"></span>
     </div>
   );
 };
