a61530912590a5b9154b26c56c967bc8da9de8ed
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-03 20:18:07 +0600

    loading updated #2

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex items-center z-50 justify-center h-screen bg-gray-100">
-      <div className="w-64 bg-gray-300 rounded-full h-6"></div>
-    </div>
+    <div className="flex items-center z-50 justify-center h-screen bg-gray-100"></div>
   );
 };
 
