ea36800a52405536bec8851a2f59ccb6af0768d7
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-04 11:54:19 +0600

    ratings component created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+import Ratings from "./Ratings";
 
 export default function Card({ mobile }) {
   const [quantity, setQuantity] = useState(1); // Initialize the quantity state
@@ -60,103 +61,7 @@ export default function Card({ mobile }) {
         </div>
         <div className="text-xl font-semibold">${mobile.price}</div>
         <div className="flex flex-wrap gap-2 ">
-          <div className="rating cursor-default rating-md rating-half">
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="rating-hidden"
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-1 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings >= 0 && mobile.ratings <= 0.5 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-2 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings >= 0.5 && mobile.ratings <= 1 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-1 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings >= 1 && mobile.ratings <= 1.5 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-2 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings >= 1.5 && mobile.ratings <= 2 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-1 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings >= 2 && mobile.ratings <= 2.5 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-2 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings > 2.5 && mobile.ratings <= 3 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-1 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings > 3 && mobile.ratings <= 3.5 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-2 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings > 3.5 && mobile.ratings <= 4 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-1 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings > 4 && mobile.ratings <= 4.5 ? true : false
-              }
-            />
-            <input
-              type="radio"
-              name={`rating-${mobile.id}`}
-              className="mask mask-star cursor-default mask-half-2 bg-orange-400"
-              disabled
-              checked={
-                mobile.ratings > 4.5 && mobile.ratings <= 5 ? true : false
-              }
-            />
-          </div>
+          <Ratings mobile={mobile} />
           <div>({mobile.ratings})</div>
         </div>
         <p>{words}...</p>
