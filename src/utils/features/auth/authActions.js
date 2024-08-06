c7895b65383eaa0662b7f59d729ec73d33d9fd28
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-06 21:02:34 +0600

    backend url adding

---
+++ src/utils/features/auth/authActions.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+
+export const RegisterUsers = () => {
+  const axiosPublic = useAxiosPublic();
+
+  return createAsyncThunk(
+    "auth/register",
+    async (
+      { firstName, lastName, dateOfBirth, gender, email, password },
+      { rejectWithValue }
+    ) => {
+      {
+        try {
+          const config = {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          };
+          await axiosPublic.post(
+            `${backendURL}/api/user/register`,
+            { firstName, lastName, dateOfBirth, gender, email, password },
+            config
+          );
+        } catch (error) {
+          // return custom error message from backend if present
+          if (error.response && error.response.data.message) {
+            return rejectWithValue(error.response.data.message);
+          } else {
+            return rejectWithValue(error.message);
+          }
+        }
+      }
+    }
+  );
+};
