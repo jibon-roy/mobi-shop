e803d2a894ed22f885e99269f0ed8701114eac78
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-09 01:59:58 +0600

    public router add

--- src/utils/features/routes/PublicRouter.jsx
+++ src/utils/features/routes/PublicRouter.jsx
@@ -1,3 +1,11 @@
-export default function PublicRouter() {
-  return <div>PublicRouter</div>;
+import useUserActions from "../../hooks/useUserActions";
+
+export default function PublicRouter({ children }) {
+  const { user, logOut } = useUserActions();
+
+  if (!user) {
+    return children;
+  } else {
+    logOut();
+  }
 }
